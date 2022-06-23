42ae2a24388c5e8938b7b91ec5c62e36738a03bd
Mardionocs <mardionoputra19>
2022-06-24 01:42:46 +0700

    update app

--- src/App.js
+++ src/App.js
@@ -4,12 +4,7 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <h1>Selamat Datang di My To Do List App</h1>
-        <p>
-          Hallo ini adalah aplikasi todo list!
-        </p>
-      </header>
+     
     </div>
   );
 }
