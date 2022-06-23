f0c0cf4aff4339879844044f98648521b5163a59
Mardionocs <mardionoputra19>
2022-06-24 02:28:22 +0700

    update tampilan menggunakan bootstrap #1

--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -3,7 +3,13 @@ import React from 'react';
 const TodoList = () => {
     return (
         <div>
-            
+            <div className='header text-center'>
+                <h3>Todo List</h3>
+                <button className="btn btn-primary">Create Task</button>
+            </div>
+            <div className='task-container'>
+
+            </div>
         </div>
     );
 };
